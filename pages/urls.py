ec0716e6bdd65e8720e349642eda59a9ce3764fc
NadaKhaled157 <nadakhaled9277@gmail.com>
2024-05-06 21:27:57 +0300

    login feature semi-final2

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,4 @@ urlpatterns=[
 path("", views.index, name="registeration-page"),
 path("login", views.login, name="login-page"),
 path('authenticate/', views.authenticate_user, name='authenticate_user'),
-path('test', views.test_db) #DELETE
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.db import connection
+from django.contrib.auth.hashers import check_password
 
-from .forms import UserInputForm
 # Create your views here.
 
 def create_table():
@@ -22,60 +22,6 @@ def index(request):
 def login(request):
     return render(request, "pages/login.html")
 
-def test_db(request): #DELETE
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT * from users')
-        data = cursor.fetchall()
-    return render(request, 'pages/test_db.html',{'data': data})
-
-
-# def get_user_input(request):
-#     if request.method == 'POST':
-#         form = UserInputForm(request.POST)
-#         if form.is_valid():
-#             # Process the data in form.cleaned_data
-#             data = form.cleaned_data['user_data']
-#             # Do something with the data
-#             ...
-#             return render(request, 'success.html')  # Redirect after POST
-#     else:
-#         form = UserInputForm()  # An unbound form
-
-#     return render(request, 'input_form.html', {'form': form})
-
-from django.db import connection
-from django.contrib.auth.hashers import check_password
-
-# def authenticate_user(request):
-#     # Create a new database cursor
-#         if request.method == 'POST':
-#             email = request.POST.get('email')
-#             password = request.POST.get('password')
-#         with connection.cursor() as cursor:
-#         # Execute the raw SQL query
-#             cursor.execute("SELECT * FROM users WHERE email = %s", [email])
-#             user = cursor.fetchone()
-
-#         # Check if a user with the provided email exists
-#             if user:
-#             # user[5] is the index of the password field
-#                 hashed_password = user[5]
-            
-#             # Use Django's check_password method to compare the passwords
-#                 if check_password(password, hashed_password):
-#                 # Password is correct, return the user object
-#                     return HttpResponse('Welcome Back')
-#                 else:
-#                 # Password is incorrect
-#                     return HttpResponseNotFound('Password is incorrect')
-#             else:
-#             # No user found with the provided username
-#                 return HttpResponseNotFound("User doesn't exist")
-
-from django.db import connection
-from django.http import HttpResponse, HttpResponseNotFound
-from django.contrib.auth.hashers import check_password
-
 def authenticate_user(request):
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -88,7 +34,7 @@ def authenticate_user(request):
 
         if user:
             # user[6] is the index of the password field
-            hashed_password = user[6]
+            hashed_password = user[6] #change it so the 6 is not hardcoded
 
             # Use Django's check_password method to compare the passwords
             if check_password(password, hashed_password):
@@ -96,37 +42,16 @@ def authenticate_user(request):
                 return HttpResponse('Welcome Back')
             else:
                 # Password is incorrect
-                return HttpResponse(f"Entered Password: {password} and Database Password: {hashed_password}")
+                # context = {"error" : "The password you entered is incorrect."}
+                # return render(request, "../login.html", context)
+                return HttpResponse("""    <div>
+                                            <h1 style='color:rgb(200,0,0)'>The password you entered is incorrect.</h1>
+                                            </div>""")
         else:
             # No user found with the provided email
-            return HttpResponseNotFound("User doesn't exist")
+            return HttpResponse("""    <div>
+                                            <h1 style='color:rgb(200,0,0)'>This user does not exist.<br>
+                                            Please check your email or sign up for a new account.</h1>
+                                            </div>""")
 
-
-from django.shortcuts import render, redirect
-# from .models import User
-from django.contrib.auth.hashers import make_password
-
-# def create_user(request):
-#     email = "hashtest@gmail.com"
-#     raw_password = '12345'  # Get the raw password
-
-#         # Hash the password
-#     hashed_password = make_password(raw_password)
-
-#         # Create a new user
-#     sql = "INSERT INTO users (fname, lname, email, address, phone_number, password) VALUES ('hash','test', %s, '123 Street', '010', %s)"
-#     with connection.cursor() as cursor:
-#         cursor.execute(sql, [email, hashed_password])
-#         connection.commit()
-#     # user = User(fname = 'hash', lname='test',address='123 Street', phone_number='010', email=email, password=hashed_password)
-#     # user.save()
-#     return HttpResponse("Temporary user created") # Render your registration form template
-
-
-# # Usage
-# user = authenticate_user('my_username', 'my_password')
-# if user:
-#     print("User authenticated successfully!")
-# else:
-#     print("Authentication failed.")
 
